d73144dda7765ac4f7d568893373a4ec8ad6cfb0
Evan <gevan73@gmail.com>
2020-07-02 19:38:24 +0200

    Copyright update

--- wallet/src/main/splash-controller.js
+++ wallet/src/main/splash-controller.js
@@ -1,7 +1,8 @@
 /*
  * This file is part of The UNIGRID Wallet
  * Copyright (C) 2019 The Swipp developers <info@swippcoin.com>
- *
+ * Copyright (C) 2020 The UNIGRID organization
+ * 
  * The UNIGRID Wallet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
