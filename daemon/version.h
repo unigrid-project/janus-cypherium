e02e86583e69b068f67a7afa2215bc5df5fa1ba0
Evan <gevan73@gmail.com>
2020-07-02 13:37:36 +0200

    Include copyright for Swipp and implement ComparableVersion

--- daemon/version.h
+++ daemon/version.h
@@ -1,7 +1,8 @@
 // Copyright (c) 2012-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2018 The PIVX developers
-// Copyright (c) 2018-2019 The UNIGRID organization
+// Copyright (c) 2018-2020 The UNIGRID organization
+// Copyright (c) 2017-2018 The Swipp developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -41,5 +42,53 @@ static const int MEMPOOL_GD_VERSION = 60002;
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
 static const int NO_BLOOM_VERSION = 70005;
 
+struct ComparableVersion
+{
+    int major = 0, minor = 0, revision = 0, build = 0;
 
+    ComparableVersion(std::string version)
+    {
+        std::sscanf(version.c_str(), "%d.%d.%d.%d", &major, &minor, &revision, &build);
+    }
+
+    bool operator < (const ComparableVersion& other) const
+    {
+        if (major < other.major)
+            return true;
+        else if (minor < other.minor)
+            return true;
+        else if (revision < other.revision)
+            return true;
+        else if (build < other.build)
+            return true;
+        return false;
+    }
+
+    bool operator == (const ComparableVersion& other)
+    {
+        return major == other.major
+            && minor == other.minor
+            && revision == other.revision
+            && build == other.build;
+    }
+
+    friend std::ostream& operator << (std::ostream& stream, const ComparableVersion& ver)
+    {
+        stream << ver.major;
+        stream << '.';
+        stream << ver.minor;
+        stream << '.';
+        stream << ver.revision;
+        stream << '.';
+        stream << ver.build;
+        return stream;
+    }
+
+    std::string ToString() const
+    {
+        std::stringstream s;
+        s << major << "." << minor << "." << revision << "." << build;
+        return s.str();
+    }
+};
 #endif // BITCOIN_VERSION_H
