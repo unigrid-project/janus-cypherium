d45b5af98c1d3338dbea8f72d33b53592c0de95a
Evan <gevan73@gmail.com>
2020-07-06 15:46:51 +0200

    Created a few more page sections for explorer and masternodes.

---
+++ wallet/src/renderer/masternodes/masternodes-content.js
@@ -0,0 +1,31 @@
+/*
+ * This file is part of The UNIGRID Wallet
+ * Copyright (C) 2019 The Swipp developers <info@swippcoin.com>
+ *
+ * The UNIGRID Wallet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The UNIGRID Wallet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import Content from "../content";
+
+export default class MasternodesContent extends React.Component {
+	render() {
+		return(
+			<Content id="masternodes">
+				<div>masternodes</div><div></div>
+			</Content>
+		);
+	}
+}
+
