35c5016389195b1d5dce9228f19f5c7c38f9947c
Evan <gevan73@gmail.com>
2020-07-10 16:05:27 +0200

    Transaction component for displaying latest transactions on the main screen

--- wallet/src/common/components/Transaction.js
+++ wallet/src/common/components/Transaction.js
@@ -18,16 +18,110 @@
 
 import React, { useState } from "react";
 import "./Transaction.css";
+import { faSignInAlt, faSignOutAlt, faCoins, faClock, faCompass } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import Tooltip from "react-simple-tooltip"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { css } from "styled-components"
+library.add(faSignInAlt, faSignOutAlt, faCoins, faClock);
 
-function Transaction({ data }) {
+function Transaction({ data, index }) {
     const [amount] = useState(data.amount);
 
     return (
         <div className="transaction--main">
-            <div>{data.amount}</div>
-            <div>{data.category}</div>
-            <div>{data.address}</div>
-        </div>
+            <div className="">
+                <div className="circle">
+                    {getArrayIndex(index)}
+                </div></div>
+            <div className="pad--divs">
+                <Tooltip
+                    arrow={10}
+                    zIndex={200}
+                    fadeDuration={150}
+                    radius={10}
+                    fontFamily='Roboto'
+                    fontSize='5'
+                    fadeEasing="linear"
+                    background="rgba(0, 0, 0, 0.85)"
+                    content={calculateDateFromEpoch(data.timereceived)}
+                    customCss={css`
+                    white-space: nowrap;
+                  `}
+                >
+                    <FontAwesomeIcon size="lg" icon={faClock} color="white" />
+                </Tooltip>
+            </div >
+            <div className="pad--divs">
+                <Tooltip
+                    arrow={10}
+                    zIndex={200}
+                    fadeDuration={150}
+                    radius={10}
+                    fontFamily='Roboto'
+                    fontSize='5'
+                    fadeEasing="linear"
+                    background="rgba(0, 0, 0, 0.85)"
+                    content={getCategory(data.category)}
+                >{getCategroyIcon(data.category)}
+                </Tooltip>
+
+            </div>
+            <div className="pad--divs">{data.amount}</div>
+            <div className="pad--divs">
+                <Tooltip
+                    arrow={10}
+                    zIndex={200}
+                    fadeDuration={150}
+                    radius={10}
+                    fontFamily='Roboto'
+                    fontSize='5'
+                    fadeEasing="linear"
+                    background="rgba(0, 0, 0, 0.85)"
+                    content="Show in explorer"
+                    customCss={css`
+                    white-space: nowrap;
+                  `}
+                >
+                    <a href={"http://explorer.unigrid.org/tx/" + data.txid} target="_blank"><FontAwesomeIcon size="lg" icon={faCompass} color="grey" /> </a>
+                </Tooltip>
+            </div>
+        </div >
     )
+    function getCategory(cat) {
+        return cat;
+    }
+    function getArrayIndex(num) {
+        let newNum = parseInt(num);
+        return newNum + 1;
+    }
+    function calculateDateFromEpoch(epoch) {
+        var recDate = new Date(epoch * 1000);
+        const date = recDate.toISOString().split('T')[0];
+        const time = recDate.toTimeString().split(" ")[0];
+
+        return (
+            <div>
+                <div>{date}</div>
+                <div>{time}</div>
+            </div>
+        )
+    }
+    function getCategroyIcon(category) {
+        switch (category) {
+            case "receive":
+                return <FontAwesomeIcon size="lg" icon={faSignInAlt} color="lightgreen" />
+                break;
+            case "send":
+                return <FontAwesomeIcon size="lg" icon={faSignOutAlt} color="lightsalmon" />
+                break;
+            case "stake":
+                return <FontAwesomeIcon size="lg" icon={faCoins} color="white" />
+                break;
+            default:
+                return <FontAwesomeIcon size="lg" icon={faSignInAlt} color="white" />
+                break;
+        }
+    }
 }
 export default Transaction;
