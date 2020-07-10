84a627b03a6ac680079b30f4e8e877cdaaa2c139
Evan <gevan73@gmail.com>
2020-07-10 10:02:12 +0200

    started creating transaction components that can hold transactions for the homescreen and possibly also transaction view?

---
+++ wallet/src/common/components/Transaction.js
@@ -0,0 +1,33 @@
+/*
+ * This file is part of The UNIGRID Wallet
+ * Copyright (C) 2019 The Swipp developers <info@swippcoin.com>
+ *
+ * The UNIGRID Wallet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The UNIGRID Wallet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React, { useState } from "react";
+import "./Transaction.css";
+
+function Transaction({ data }) {
+    const [amount] = useState(data.amount);
+
+    return (
+        <div className="transaction--main">
+            <div>{data.amount}</div>
+            <div>{data.category}</div>
+            <div>{data.address}</div>
+        </div>
+    )
+}
+export default Transaction;
