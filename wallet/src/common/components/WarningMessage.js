117b85b1cac0e3e9a932229755efddad543eaee6
Evan <gevan73@gmail.com>
2020-07-14 14:39:39 +0200

    Warning notification component. This can be imported and used anywhere with a custom message.

---
+++ wallet/src/common/components/WarningMessage.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of The UNIGRID Wallet
+ * Copyright (C) 2020 The UNIGRID Organization
+ *
+ * The UNIGRID Wallet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The UNIGRID Wallet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React, { useState, useEffect } from "react";
+
+function WarningMessage({ message, onAnimationComplete, startAnimation }) {
+    const [errorClasses, setErrorClasses] = useState(startAnimation);
+
+    return (
+        <div className={errorClasses} onAnimationEnd={onErrorEnd}>
+            <div className="error--text padding">{message}</div>
+        </div>
+    )
+
+    function onErrorEnd() {
+        onAnimationComplete();
+        setErrorClasses("error--text-start");
+    }
+}
+
+export default WarningMessage;
