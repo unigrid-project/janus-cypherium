98cd44bb42b0ab49efaeba7c229ee14ba74d5328
Evan <gevan73@gmail.com>
2020-07-14 14:43:48 +0200

    Send container that renders inputs for addings addresses and amounts. Data is passed up to the parent that implements it. Disabled the option to add extra recipients at this time as sendmany is not working with accounts.

---
+++ wallet/src/common/components/Send.js
@@ -0,0 +1,127 @@
+/*
+ * This file is part of The UNIGRID Wallet
+ * Copyright (C) 2020 The UNIGRID Organization
+ *
+ * The UNIGRID Wallet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The UNIGRID Wallet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React, { useState, useEffect } from "react";
+import Expand from "react-expand-animated";
+import EnterField from "./EnterField";
+import Button from "./Button";
+import "./Send.css"
+import SendInputs from "./SendInputs";
+import _ from "lodash";
+
+function Send({
+    setSendAddress,
+    sendCoins,
+    cancelSendOperation,
+    resetState,
+    setSendAmount,
+    defaultValues
+}) {
+    const [inputValues, setInputValues] = useState({});
+    const [recipients, setRecipients] = useState(defaultValues);
+    const [rerender, setRerender] = useState(false);
+    const [recipientCounter, setRecipientCounter] = useState(2);
+    useEffect(() => {
+        //console.log("updating state ", recipients);
+    });
+    useEffect(() => {
+        setRecipients(defaultValues);
+    }, [defaultValues]);
+    return (
+        <div
+            key={Object.keys(recipients).length}
+            className="send--inner--container">
+            {createRecipients()}
+            <div className="btn--row--send">
+                <Button
+                    buttonStyle="btn--secondary--solid"
+                    buttonSize="btn--small"
+                    handleClick={() => sendCoins(recipients)}>SEND</Button>
+                <Button
+                    buttonStyle="btn--secondary--solid"
+                    buttonSize="btn--small"
+                    handleClick={() => onCancelPressed()}>CANCEL</Button>
+            </div>
+        </div>
+    )
+    /* <Button
+                   buttonStyle="btn--secondary--solid"
+                   buttonSize="btn--small"
+                   handleClick={() => addRecipient()}>ADD RECIPIENT</Button> */
+    function onCancelPressed() {
+        setRecipients({ "address1": { "address": "", "amount": "" } });
+        setRecipientCounter(2);
+        cancelSendOperation();
+    }
+    function addRecipient() {
+        const key = "address".concat(recipientCounter);
+        setRecipientCounter(recipientCounter + 1);
+        setRecipients(Object.assign(recipients, { [key]: { "address": "", "amount": "" } }));
+        setRerender(!rerender);
+    }
+    function removeRecipient(e) {
+        console.log("remove: ", e);
+        setRecipients(_.omit(recipients, e));
+        setRerender(!rerender);
+    }
+    function setSendAddress(v, recipientKey) {
+        const updateAddress = recipients;
+        Object.keys(updateAddress).map(key => {
+            if (key === recipientKey) {
+                updateAddress[key].address = v;
+            }
+        })
+        // console.log("updateAddress ", updateAddress);
+        setRecipients(updateAddress);
+    }
+
+    function setSendAmount(v, recipientKey) {
+        const updateAmount = recipients;
+        Object.keys(updateAmount).map(key => {
+            if (key === recipientKey) {
+                updateAmount[key].amount = v;
+            }
+        })
+        // console.log("updateAmount ", updateAmount);
+        setRecipients(updateAmount);
+    }
+    function createRecipient(key) {
+        const showRemove = key !== "address1";
+        console.log("render address", recipients[key].address);
+        console.log("render amount", recipients[key].amount);
+        const amount = recipients[key].amount;
+        const address = recipients[key].address;
+        return (
+            <SendInputs
+                showRemove={showRemove}
+                key={key}
+                removeRecipient={(e) => removeRecipient(e)}
+                setSendAddress={setSendAddress}
+                setSendAmount={setSendAmount}
+                recipientKey={key}
+                inputValueAmount={amount}
+                inputValueAddress={address}
+            />
+        )
+    }
+    function createRecipients() {
+        return Object.keys(recipients).map((key) => createRecipient(key))
+    }
+}
+
+export default Send;
---
+++ wallet/src/common/components/SendInputs.js
@@ -0,0 +1,102 @@
+/*
+ * This file is part of The UNIGRID Wallet
+ * Copyright (C) 2020 The UNIGRID Organization
+ *
+ * The UNIGRID Wallet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The UNIGRID Wallet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React, { useState, useEffect } from "react";
+import EnterField from "./EnterField";
+import Button from "./Button";
+import "./Send.css"
+
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import RPCClient from "common/rpc-client.js";
+import WarningMessage from "./WarningMessage";
+library.add(faTimesCircle);
+
+function SendInputs({
+    inputValueAmount,
+    inputValueAddress,
+    setSendAddress,
+    showRemove,
+    setSendAmount,
+    recipientKey,
+    removeRecipient,
+}) {
+    const [warningMessage, setWarningMessage] = useState("");
+
+    return (
+        <div className="send--input--container">
+            <div>
+                <EnterField
+                    key={inputValueAddress+"address"}
+                    type={"text"}
+                    clearField={inputValueAddress}
+                    myStyle={"addressInput"}
+                    onBlurOut={onBlurOutAddress}
+                    placeHolder="Address"
+                    updateEntry={(v) => setSendAddress(v, recipientKey)}
+                />
+                <EnterField
+                    placeHolder="Amount"
+                    key={inputValueAmount+"amount"}
+                    type={"float"}
+                    clearField={inputValueAmount}
+                    myStyle={"smallInput"}
+                    updateEntry={(v) => setSendAmount(v, recipientKey)}
+                />
+                {showRemove === true ?
+                    <div onClick={() => removeRecipient(recipientKey)}>
+                        <FontAwesomeIcon
+                            size="lg"
+                            icon={faTimesCircle} color="red" />
+                    </div>
+                    :
+                    null}
+            </div>
+            {warningMessage !== "" ? renderWarning() : null}
+        </div>
+    )
+    function renderWarning() {
+        return (
+            <WarningMessage
+                className="warning--position"
+                message={warningMessage}
+                onAnimationComplete={onAnimationComplete}
+                startAnimation="error--text-start error--text--animation" />
+        )
+    }
+    function onAnimationComplete() {
+        setWarningMessage("");
+    }
+    async function onBlurOutAddress(e) {
+        console.log(e.target.value);
+        var rpcClient = new RPCClient();
+        const address = e.target.value;
+        Promise.all([
+            rpcClient.validateAddress([address]),
+            new Promise(resolve => setTimeout(resolve, 500))
+        ]).then((response) => {
+            if (!response[0].isvalid) setWarningMessage("Address is not valid!");
+        }, (stderr) => {
+            console.error(stderr);
+        });
+    }
+
+}
+
+export default SendInputs;
