06e072fc06999e8c8cda908652dbe60821c701a0
Evan <gevan73@gmail.com>
2020-07-19 07:56:29 +0200

    testing compile with boost beast temporary certificate to remove. Added websocket to makefile and started working on header file

---
+++ daemon/websocket_notification_publisher.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2018-2020 The UNIGRID organization
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UNIGRID_WEBSOCKET_H
+#define UNIGRID_WEBSOCKET_H
+
+
+
+#endif
