a9e14fbf91483654cb0f62211f7d056867b47baf
Evan <gevan73@gmail.com>
2020-07-19 07:52:06 +0200

    option to encrypt wallet from settings screen which will restart wallet on complete. TODO button should be disabled if encrypted and enabled if not doesnt seem to always work as intended.

--- wallet/src/renderer/settings/settings-content.js
+++ wallet/src/renderer/settings/settings-content.js
@@ -17,16 +17,166 @@
  * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Content from "../content";
+import Store from "electron-store";
+import Button from "../../common/components/Button";
+import WarningMessage from "../../common/components/WarningMessage";
+import Expand from "react-expand-animated";
+import EnterField from "../../common/components/EnterField";
+import "./settings-content.css";
+import RPCClient from "../../common/rpc-client.js";
+import { ipcRenderer, remote } from "electron";
+import { sendDesktopNotification } from "../../common/components/DesktopNotifications";
 
-export default class SettingsContent extends React.Component {
-	render() {
-		return(
-			<Content id="settings">
-				<div>Settings</div><div></div>
-			</Content>
-		);
+const store = new Store();
+
+function SettingsContent(props) {
+	const [isEncrypted, setIsEncrypted] = useState(false);
+	const [openEncrypt, setOpenEncrypt] = useState(false);
+	const [passphrase, setPassphrase] = useState("");
+	const [repeatPassphrase, setRepeatPassphrase] = useState("");
+	const [warningMessage, setWarningMessage] = useState("");
+	const [passKey, setPassKey] = useState();
+	const [repeatKey, setRepeatKey] = useState();
+	const [encryptKey, setEncryptKey] = useState();
+	const [encryptingWallet, setEncryptingWallet] = useState(false);
+	useEffect(() => {
+		console.log("getting store: ");
+		setIsEncrypted(store.get("encrypted"));
+	}, [])
+	useEffect(() => {
+		console.log("isEncrypted: ", isEncrypted);
+
+		setEncryptKey(Math.random());
+	}, [isEncrypted])
+	useEffect(() => {
+		resetPassphraseContainer();
+	}, [openEncrypt, encryptingWallet])
+
+	return (
+		<Content id="settings">
+			<div className="main--settings-container" >
+				<div>Settings</div>
+
+				<Button
+					key={encryptKey}
+					handleClick={() => setOpenEncrypt(!openEncrypt)}
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid"
+					disabled={isEncrypted}>Encrypt Wallet</Button>
+				<div>
+					{renderEncryptWallet()}
+				</div>
+
+				<Button
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid">Backup Wallet</Button>
+				<Button
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid">Dump Wallet</Button>
+				<Button
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid">Dump Private Key</Button>
+				<Button
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid">Open Configuration</Button>
+				<Button
+					buttonSize="btn--tiny"
+					buttonStyle="btn--secondary--solid">Open Masternode Configuration</Button>
+			</div>
+		</Content>
+	);
+
+	function resetPassphraseContainer() {
+		setRepeatPassphrase("");
+		setPassphrase("");
+		setPassKey(Math.random());
+		setRepeatKey(Math.random());
+	}
+	async function submitEncryptWallet() {
+		if (passphrase !== repeatPassphrase) {
+			setWarningMessage("Passphrases do not match!");
+			setRepeatPassphrase("");
+			setPassphrase("");
+			setPassKey(Math.random());
+			setRepeatKey(Math.random());
+			return;
+		}
+
+		ipcRenderer.sendTo(remote.getCurrentWebContents().id, "state", "working");
+		setEncryptingWallet(!encryptingWallet);
+		var rpcClient = new RPCClient();
+		Promise.all([
+			rpcClient.encryptWallet([passphrase]),
+			new Promise(resolve => setTimeout(resolve, 500))
+		]).then((response) => {
+
+			sendDesktopNotification(response[0]);
+			ipcRenderer.sendTo(remote.getCurrentWebContents().id, "state", "completed");
+			setEncryptingWallet(!encryptingWallet);
+			ipcRenderer.send("wallet-restart");
+
+		}).catch((error) => {
+			console.log("encryptWallet error")
+			console.error(error.message);
+			ipcRenderer.sendTo(remote.getCurrentWebContents().id, "state", "completed");
+			setEncryptingWallet(!encryptingWallet);
+		});
+	}
+	function renderEncryptWallet() {
+		if (!openEncrypt) return null
+		return (
+			<Expand open={openEncrypt}>
+				<div className="input--container" key={passKey}>
+					<div>Enter a passphrase for the wallet.</div>
+					<div>Please use a passphrase of 10 or more random characters or a minimum of 8 random words.</div>
+					<div>After confirming your wallet will be encrypted then auto restarted.</div>
+					<div className="input--fields">
+						<EnterField
+
+							placeHolder="Passphrase"
+							type={"password"}
+							clearField={passphrase}
+							myStyle={"medium--input"}
+							updateEntry={(v) => setPassphrase(v)} />
+						<EnterField
+							key={repeatKey}
+							placeHolder="Repeat Passphrase"
+							type={"password"}
+							clearField={repeatPassphrase}
+							myStyle={"medium--input"}
+							updateEntry={(v) => setRepeatPassphrase(v)} />
+					</div>
+					<div className="confirm-btns">
+						<Button
+							disabled={encryptingWallet}
+							handleClick={() => submitEncryptWallet()}
+							buttonSize="btn--tiny"
+							buttonStyle="btn--success--solid">Confirm</Button>
+						<Button
+							disabled={encryptingWallet}
+							handleClick={() => setOpenEncrypt(!openEncrypt)}
+							buttonSize="btn--tiny"
+							buttonStyle="btn--warning--solid">Cancel</Button>
+						{warningMessage !== "" ? renderWarning() : null}
+					</div>
+				</div>
+			</Expand >
+		)
+	}
+	function renderWarning() {
+		return (
+			<WarningMessage
+				message={warningMessage}
+				onAnimationComplete={onAnimationComplete}
+				startAnimation="error--text-start error--text--animation" />
+		)
+	}
+	function onAnimationComplete() {
+		setWarningMessage("");
 	}
 }
+
+export default SettingsContent;
 
