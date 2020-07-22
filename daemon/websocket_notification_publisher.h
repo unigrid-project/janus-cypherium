36364258555e1901defec130ce7b901cebf953c6
Evan <gevan73@gmail.com>
2020-07-22 20:37:37 +0200

    reverted websocket to singleton class with boost beast.

--- daemon/websocket_notification_publisher.h
+++ daemon/websocket_notification_publisher.h
@@ -5,6 +5,21 @@
 #ifndef UNIGRID_WEBSOCKET_H
 #define UNIGRID_WEBSOCKET_H
 
+#include <boost/asio.hpp>
+#include <boost/asio/coroutine.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <iostream>
+
+class WebSocketNotificationPublisher;
+
+class WebSocketNotificationPublisher : public std::enable_shared_from_this<WebSocketNotificationPublisher>
+{
+}
+
 
 
 #endif
