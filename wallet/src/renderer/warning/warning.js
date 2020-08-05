fd22c34d713c167b27168fe97a9b17eedbe77db6
Evan <gevan73@gmail.com>
2020-08-05 11:45:27 +0200

    Auto Updater: git url was missing a  for version.

--- wallet/src/renderer/warning/warning.js
+++ wallet/src/renderer/warning/warning.js
@@ -42,7 +42,7 @@ function Warning() {
 			setVersion(data.version);
 			setMessage(data.message);
 			setTitle(data.title);
-			seetUrlLink(githubLink.concat("/releases/tag/").concat(data.version));
+			seetUrlLink(githubLink.concat("/releases/tag/v").concat(data.version));
 		});
 	}, []);
 
