eebeaf810855838cbc4ad3f5d7f5c3f7d6ad7fee
Evan Green <gevan73@gmail.com>
2020-12-16 13:03:06 +0200

    Config file: we now import all the consts from the local config.json. The data is then placed in a local store for easy retrieval. The init returns a promise to determine whether everything has loaded correctly on start. Each const is returned via a get inside the config.

--- wallet/src/common/config.js
+++ wallet/src/common/config.js
@@ -16,20 +16,108 @@
  * along with The UNIGRID Wallet. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import File from "./file";
 import Store from "electron-store";
 
 const store = new Store();
 
 export default class Config {
+
     /* Import default conts from config.json */
     /* That data is then stored in electron-store */
     /* which can be accesed from anywhere without another import */
-    static loadAppConfig() {
-        const fs = require('fs');
-        const path = require('path');
-        let config = fs.readFileSync(path.join(__static, '/config.json'))
-        let data = JSON.parse(config);
-        store.set(data);
+    //static random = "somerandomshit";
+
+    static loadAppConfig(window) {
+        return new Promise((resolve, reject) => {
+            try {
+                const fs = require('fs');
+                const path = require('path');
+                let config = fs.readFileSync(path.join(__static, '/config.json'));
+                let data = JSON.parse(config);
+                store.set(data);
+
+                if (this.getStore()) {
+                    return resolve();
+                } else {
+                    window.webContents.send("fatal-error", "Error loading config file");
+                    window.webContents.send("state", "idle");
+                    reject("Error loading config file");
+                }
+            } catch {
+                window.webContents.send("fatal-error", "Error loading config file");
+                window.webContents.send("state", "idle");
+                reject("Error loading config file");
+            }
+        });
+    }
+
+    static async initConfig(window) {
+        var p;
+        p = await Config.loadAppConfig(window);
+        return p;
+    }
+
+    static getStore() {
+        return store.get("janusConfirguration");
+    }
+
+    static getDiscordLink() {
+        return this.getStore().discordLink;
+    }
+
+    static getTwitterLink() {
+        return this.getStore().twitterLink;
+    }
+
+    static getTelegramLink() {
+        return this.getStore().telegramLink;
+    }
+
+    static getExplorerLink() {
+        return this.getStore().explorerLink;
+    }
+
+    static getHomepage() {
+        return this.getStore().homePage;
+    }
+
+    static getGithubLink() {
+        return this.getStore().githubLink;
+    }
+
+    static getProjectName() {
+        return this.getStore().projectName;
+    }
+
+    static getConfFile() {
+        return this.getStore().confFile;
+    }
+
+    static getMasternodeFile() {
+        return this.getStore().masternodeFile;
+    }
+
+    static getIsMasternode() {
+        return this.getStore().isMasternode;
+    }
+
+    static getHasStaking() {
+        return this.getStore().hasStaking;
+    }
+
+    static getIsDaemonLocal() {
+        return this.getStore().isDaemonLocal;
+    }
+
+    static getSplashHTML() {
+        return this.getStore().splashInfo.html;
+    }
+
+    static getUserModelId() {
+        return this.getStore().userModelId;
+    }
+
+    static getMasternodeSetupScript() {
+        return `bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/unigrid-project/masternode-setup/master/unigridd.sh)" ; source ~/.bashrc`;
     }
 }
