d4be7e11772bf88ce8945087f6ae1a6c8090ff1b
dekm0101 <evan@unigrid.org>
2021-03-12 09:55:52 +0200

    Address: convert addresses removing CPH and 0x

--- wallet/src/common/walletutils/WalletService.js
+++ wallet/src/common/walletutils/WalletService.js
@@ -335,7 +335,7 @@ export class WalletService {
     convertAddr(addr) {
         if (addr.substring(0, 3) === "CPH") {
             return addr.replace('CPH', '');
-        } else if (addr.substring(0, 3) === "0x") {
+        } else if (addr.substring(0, 2) === "0x") {
             return addr.replace('0x', '');
         } else if (addr.substring(0, 3) === "cph") {
             return addr.replace('cph', '');
